fbcefab85cb20eaf42081a47bfc168690bdb9a10
Maxounn <magz.hofmangmail.com>
2023-06-26 15:05:38 +0200

    uz me to sere

--- Maxa/cheer.java
+++ Maxa/cheer.java
@@ -2,7 +2,7 @@ package Maxa;
 
 public class cheer {
     public void cheer(String cheer, String eternalPain) {
-        cheer = "Tak to blahopreji ("+cheer+")" + eternalPain;
+        cheer = "Tak to blahopreji ("+cheer+")" + eternalPain + "kill me pls";
         System.out.println(cheer);
     }
 }
