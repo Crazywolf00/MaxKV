bb3e972d7856b72e7532770ed1a35925e81d3b1b
Vaclav Vlcek <Vaclav.Vlcek@seznam.cz>
2023-06-26 14:58:24 +0200

    a5

--- vaclav/Greet.java
+++ vaclav/Greet.java
@@ -6,7 +6,7 @@ public class Greet {
 
 
     public void greeting(String name, String sirName) {
-        System.out.println(name + sirName);
+        System.out.println(name + sirName + " tvoje máma");
         System.out.println("Greetings!");
     }
 
