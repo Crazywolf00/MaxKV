389b6dcc631b6a5d5bd238d4b9eb77eca640bb38
Maxounn <magz.hofmangmail.com>
2023-06-26 15:11:12 +0200

    c5 natvrdo

--- Maxa/cheer.java
+++ Maxa/cheer.java
@@ -1,8 +1,8 @@
 package Maxa;
 
 public class cheer {
-    public void cheer(String cheer) {
-        cheer = "Tak to blahopreji (" + cheer + ")";
+    public void cheer(String cheer, String eternalPain) {
+        cheer = "Tak to blahopreji ("+cheer+")" + eternalPain;
         System.out.println(cheer);
     }
 }
